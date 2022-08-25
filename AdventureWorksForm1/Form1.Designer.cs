690f37a19b8cc9d3448ace0876bd205207fef992
davide-info <83416046+davide-info@users.noreply.github.com>
2022-08-25 17:44:03 +0200

    Aggiungere i file di progetto.

---
+++ AdventureWorksForm1/Form1.Designer.cs
@@ -0,0 +1,213 @@
+﻿
+namespace AdventureWorksForm1
+{
+	partial class Form1
+	{
+		/// <summary>
+		///  Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		///  Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		///  Required method for Designer support - do not modify
+		///  the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.dataGridView1 = new System.Windows.Forms.DataGridView();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.label4 = new System.Windows.Forms.Label();
+			this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+			this.label5 = new System.Windows.Forms.Label();
+			this.idTxt = new System.Windows.Forms.TextBox();
+			this.nameTxt = new System.Windows.Forms.TextBox();
+			this.costRateTxt = new System.Windows.Forms.TextBox();
+			this.availabilyTxt = new System.Windows.Forms.TextBox();
+			this.button1 = new System.Windows.Forms.Button();
+			this.updateTxt = new System.Windows.Forms.Button();
+			this.deleteTxt = new System.Windows.Forms.Button();
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// dataGridView1
+			// 
+			this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			this.dataGridView1.Location = new System.Drawing.Point(500, 72);
+			this.dataGridView1.Name = "dataGridView1";
+			this.dataGridView1.RowTemplate.Height = 25;
+			this.dataGridView1.Size = new System.Drawing.Size(240, 232);
+			this.dataGridView1.TabIndex = 0;
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(13, 26);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(66, 15);
+			this.label1.TabIndex = 1;
+			this.label1.Text = "Location id";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(13, 58);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(39, 15);
+			this.label2.TabIndex = 2;
+			this.label2.Text = "Name";
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Location = new System.Drawing.Point(13, 94);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(57, 15);
+			this.label3.TabIndex = 3;
+			this.label3.Text = "Cost Rate";
+			// 
+			// label4
+			// 
+			this.label4.AutoSize = true;
+			this.label4.Location = new System.Drawing.Point(13, 128);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(62, 15);
+			this.label4.TabIndex = 4;
+			this.label4.Text = "Availabilty";
+			// 
+			// dateTimePicker1
+			// 
+			this.dateTimePicker1.Location = new System.Drawing.Point(101, 157);
+			this.dateTimePicker1.Name = "dateTimePicker1";
+			this.dateTimePicker1.Size = new System.Drawing.Size(200, 23);
+			this.dateTimePicker1.TabIndex = 5;
+			// 
+			// label5
+			// 
+			this.label5.AutoSize = true;
+			this.label5.Location = new System.Drawing.Point(13, 163);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(82, 15);
+			this.label5.TabIndex = 6;
+			this.label5.Text = "Modified Date";
+			// 
+			// idTxt
+			// 
+			this.idTxt.Location = new System.Drawing.Point(92, 23);
+			this.idTxt.Name = "idTxt";
+			this.idTxt.Size = new System.Drawing.Size(100, 23);
+			this.idTxt.TabIndex = 7;
+			// 
+			// nameTxt
+			// 
+			this.nameTxt.Location = new System.Drawing.Point(78, 58);
+			this.nameTxt.Name = "nameTxt";
+			this.nameTxt.Size = new System.Drawing.Size(100, 23);
+			this.nameTxt.TabIndex = 8;
+			// 
+			// costRateTxt
+			// 
+			this.costRateTxt.Location = new System.Drawing.Point(78, 94);
+			this.costRateTxt.Name = "costRateTxt";
+			this.costRateTxt.Size = new System.Drawing.Size(100, 23);
+			this.costRateTxt.TabIndex = 9;
+			// 
+			// availabilyTxt
+			// 
+			this.availabilyTxt.Location = new System.Drawing.Point(78, 123);
+			this.availabilyTxt.Name = "availabilyTxt";
+			this.availabilyTxt.Size = new System.Drawing.Size(100, 23);
+			this.availabilyTxt.TabIndex = 10;
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(4, 200);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(125, 38);
+			this.button1.TabIndex = 11;
+			this.button1.Text = "AGGIORNA LOCATION";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// updateTxt
+			// 
+			this.updateTxt.Location = new System.Drawing.Point(154, 200);
+			this.updateTxt.Name = "updateTxt";
+			this.updateTxt.Size = new System.Drawing.Size(111, 38);
+			this.updateTxt.TabIndex = 12;
+			this.updateTxt.Text = "INSERISCI LOCATION";
+			this.updateTxt.UseVisualStyleBackColor = true;
+			this.updateTxt.Click += new System.EventHandler(this.button2_Click);
+			// 
+			// deleteTxt
+			// 
+			this.deleteTxt.Location = new System.Drawing.Point(283, 199);
+			this.deleteTxt.Name = "deleteTxt";
+			this.deleteTxt.Size = new System.Drawing.Size(75, 23);
+			this.deleteTxt.TabIndex = 13;
+			this.deleteTxt.Text = "ELIMINA LOCATION";
+			this.deleteTxt.UseVisualStyleBackColor = true;
+			this.deleteTxt.Click += new System.EventHandler(this.button3_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.deleteTxt);
+			this.Controls.Add(this.updateTxt);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.availabilyTxt);
+			this.Controls.Add(this.costRateTxt);
+			this.Controls.Add(this.nameTxt);
+			this.Controls.Add(this.idTxt);
+			this.Controls.Add(this.label5);
+			this.Controls.Add(this.dateTimePicker1);
+			this.Controls.Add(this.label4);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.dataGridView1);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.DataGridView dataGridView1;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.DateTimePicker dateTimePicker1;
+		private System.Windows.Forms.Label label5;
+		private System.Windows.Forms.TextBox idTxt;
+		private System.Windows.Forms.TextBox nameTxt;
+		private System.Windows.Forms.TextBox costRateTxt;
+		private System.Windows.Forms.TextBox availabilyTxt;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Button updateTxt;
+		private System.Windows.Forms.Button deleteTxt;
+	}
+}
+
---
+++ AdventureWorksForm1/Form1.cs
@@ -0,0 +1,480 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using System.Text;
+using System.Windows.Forms;
+
+
+
+namespace AdventureWorksForm1
+{
+    public partial class Form1 : Form
+    {
+
+        const string connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=AdventureWorks2016; MultipleActiveResultSets=True;Integrated Security=SSPI;";
+        const string dbName = "AdventureWorks2016";
+        public Form1()
+        {
+            InitializeComponent();
+            dateTimePicker1.ValueChanged += MyValueChanged;
+            dateTimePicker1.KeyDown += KeyDownEvent;
+
+            dateTimePicker1.Format = DateTimePickerFormat.Custom;
+            //dateTimePicker1.Value = default(DateTime);
+            dateTimePicker1.Checked = false;
+            dateTimePicker1.ShowCheckBox = false;
+
+            dateTimePicker1.CustomFormat = " ";
+            this.dataGridView1.AllowUserToAddRows = true;
+            dataGridView1.UserAddedRow += AddRowEvent;
+            dataGridView1.AllowUserToAddRowsChanged += UpdateRowEvent;
+            dataGridView1.RowsAdded += RowAddedEvent;
+            dataGridView1.CellClick += ClickCell;
+            dataGridView1.Click += ClickEvent;
+            //var result= AddLocationIntoLocationSqlTable("bar", 12, 12.20M, DateTime.Now);
+            nameTxt.Clear();
+            DisplayLocationTable();
+            // DeleteLocation(2);
+
+        }
+
+        private void ClickEvent(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedCells.Count >= 5)
+            {
+               
+
+                this.idTxt.Text = Convert.ToString(dataGridView1.SelectedCells[0].Value);
+                nameTxt.Text = dataGridView1.SelectedCells[1].Value.ToString();
+                
+
+                costRateTxt.Text = Convert.ToString(dataGridView1.SelectedCells[2].Value).Replace(",","");
+                availabilyTxt.Text = Convert.ToString(dataGridView1.SelectedCells[3].Value);
+                this.dateTimePicker1.Text = Convert.ToString(dateTimePicker1.Value);
+               
+            }
+            else
+            {
+                this.idTxt.Text = Convert.ToString(dataGridView1.CurrentRow.Cells[0].Value).Replace(",","");
+                nameTxt.Text = dataGridView1.CurrentRow.Cells[1].Value.ToString();
+                costRateTxt.Text = Convert.ToString(dataGridView1.CurrentRow.Cells[2].Value).Replace(",","");
+                availabilyTxt.Text = dataGridView1.CurrentRow.Cells[3].Value.ToString();
+                this.dateTimePicker1.Text = dataGridView1.CurrentRow.Cells[4].Value.ToString();
+            }
+
+        }
+
+
+
+        private void ClickCell(object sender, DataGridViewCellEventArgs e)
+        {
+            //.Show("QUI");
+            idTxt.Text = dataGridView1.CurrentRow.Cells[0].Value.ToString();
+            nameTxt.Text = dataGridView1.CurrentRow.Cells[1].Value.ToString();
+            costRateTxt.Text = dataGridView1.CurrentRow.Cells[2].Value.ToString();
+           availabilyTxt.Text = dataGridView1.CurrentRow.Cells[3].Value.ToString();
+            dateTimePicker1.Text = dataGridView1.CurrentRow.Cells[4].Value.ToString();
+        }
+
+        private void DeleteLocation(Int16 id)
+        {
+            using (var connection = new SqlConnection(connectionString))
+            {
+                var command = new SqlCommand("USE " + dbName, connection);
+                connection.Open();
+                command.ExecuteNonQuery();
+                command.CommandText = "DELETE FROM Production.Location WHERE LocationId=@id" +
+                    " AND LocationId  NOT IN (SELECT LocationId FROM Production.ProductInventory)";
+                command.Parameters.Add("@id", SqlDbType.SmallInt).Value = id;
+                try
+                {
+                    var numRows = command.ExecuteNonQuery();
+                    MessageBox.Show("Eliminazione Location OK " + "numero righe " + numRows);
+                }
+                catch (SqlException e)
+                {
+                    MessageBox.Show("Errore eliminazione " + e.Message);
+                }
+            }
+        }
+
+
+        private void RowAddedEvent(object sender, DataGridViewRowsAddedEventArgs e)
+        {
+
+
+        }
+
+        private void UpdateRowEvent(object sender, EventArgs e)
+        {
+
+
+            //  int currColumn = dataGridView1.CurrentCell.ColumnIndex;
+            //int currRow = dataGridView1.CurrentCell.RowIndex;
+            // string name = dataGridView1.CurrentRow.Cells[1].Value.ToString();
+
+        }
+
+        private void AddRowEvent(object sender, DataGridViewRowEventArgs e)
+        {
+
+            if (dataGridView1.CurrentRow != null)
+            {
+                nameTxt.Text = dataGridView1.CurrentRow.Cells[1].Value.ToString();
+                //Console.WriteLine(dataGridView1.CurrentRow.ToString());
+            }
+        }
+
+        private void Print()
+        {
+            var builder = new StringBuilder();
+
+            MessageBox.Show(dataGridView1.Columns.Count.ToString());
+
+
+
+            builder.AppendLine();
+            foreach (DataGridViewTextBoxColumn column in dataGridView1.Columns)
+            {
+                builder.Append(column.Name + " ");
+
+                foreach (DataGridViewRow row in dataGridView1.Rows)
+                {
+                    builder.Append(row.Cells[column.Index].Value + " ");
+                }
+                builder.AppendLine();
+            }
+            MessageBox.Show(builder.ToString());
+        }
+
+        private void KeyDownEvent(object sender, KeyEventArgs e)
+        {
+            if ((e.KeyCode == Keys.Back) || (e.KeyCode == Keys.Delete))
+            {
+                dateTimePicker1.CustomFormat = " ";
+                dateTimePicker1.ShowCheckBox = false;
+
+
+            }
+        }
+
+        private static Int16 AddLocationIntoLocationSqlTable(string nameLocation, short cost, decimal availbailty, DateTime dateTime)
+        {
+            Int16 result = 0;
+
+            using (var connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                var sqlNonQuery = "USE " + dbName;
+                SqlCommand command = new SqlCommand(sqlNonQuery, connection);
+                command.ExecuteNonQuery();
+                command.CommandText = "INSERT INTO Production.Location(Name,CostRate, Availability,ModifiedDate)" +
+
+                    " VALUES(@name, @cost, @av, @dateTime); SELECT SCOPE_IDENTITY() AS [SCOPE_IDENTITY]";
+                command.Parameters.Add("@name", SqlDbType.NVarChar, 50).Value = nameLocation;
+                command.Parameters.Add("@cost", SqlDbType.SmallMoney).Value = cost;
+                command.Parameters.Add("@av", SqlDbType.Decimal).Value = availbailty;
+                command.Parameters.Add("@dateTime", SqlDbType.DateTime).Value = dateTime;
+
+
+
+                try
+                {
+
+                    result = Convert.ToInt16(command.ExecuteScalar());
+
+
+
+                }
+                catch (Exception se)
+                {
+                    MessageBox.Show(" Tipo Eccezione catturata " + se.GetType() + "  Messaggio Eccezione " + se.Message);
+                }
+
+                return result;
+            }
+        }
+
+
+        private void MyValueChanged(object sender, EventArgs e)
+        {
+
+            dateTimePicker1.CustomFormat = "dd/MM/yyyy";
+            dateTimePicker1.Format = DateTimePickerFormat.Custom;
+            //MessageBox.Show(dateTimePicker1.Text);
+            if (DateTime.Now.ToShortDateString() == dateTimePicker1.Value.ToShortDateString())
+            {
+                dateTimePicker1.CustomFormat = "dd/MM/yyyy HH:mm";
+            }
+
+            dateTimePicker1.ShowCheckBox = true;
+
+
+        }
+
+        private void DisplayLocationTable()
+        {
+
+            using (var connection = new SqlConnection(connectionString))
+            {
+                var command = new SqlCommand("USE " + dbName, connection);
+                connection.Open();
+                command.ExecuteNonQuery();
+                command.CommandText = "SELECT * FROM Production.Location";
+                SqlDataAdapter adapter = new SqlDataAdapter(command);
+                DataTable table = new DataTable();
+                adapter.Fill(table);
+                this.dataGridView1.DataSource = table;
+
+
+            }
+            ResizeRowsColumnsDataGrid();
+
+        }
+
+        private void ResizeRowsColumnsDataGrid()
+        {
+            dataGridView1.Height = dataGridView1.Rows.GetRowsHeight(DataGridViewElementStates.None) + dataGridView1.ColumnHeadersHeight + 2;
+
+            dataGridView1.Width = dataGridView1.Columns.GetColumnsWidth(DataGridViewElementStates.None) + dataGridView1.RowHeadersWidth + 2;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           
+            if (string.IsNullOrEmpty(idTxt.Text))
+            {
+                MessageBox.Show("Non è possibile modificare la tabella");
+                Application.Exit();
+
+            }
+            var id = Convert.ToInt16(idTxt.Text);
+            Int16? cost = null;
+            decimal? availabilty = null;
+            ExtractAndValidateData(ref id, ref cost, ref availabilty);
+        }
+        private void ExtractAndValidateData(ref Int16 id, ref Int16? cost, ref decimal? availability)
+        {
+
+
+            string name = nameTxt.Text;
+
+            if (!string.IsNullOrEmpty(costRateTxt.Text))
+            {
+                try
+                {
+                    cost = Int16.Parse(costRateTxt.Text);
+                }
+                catch (FormatException e)
+                {
+                    MessageBox.Show("Errore " + e.Message);
+
+                }
+                try
+                {
+                    if (!string.IsNullOrEmpty(availabilyTxt.Text))
+                    {
+                        availability = decimal.Parse(availabilyTxt.Text);
+                    }
+
+                }
+                catch (FormatException fe)
+                {
+                    MessageBox.Show("Errore 2 " + fe.Message);
+                }
+
+
+
+
+
+                UpdateLocationSqlTable(id, name, cost, availability, dateTimePicker1.Text);
+
+
+            }
+
+
+
+
+         
+
+        }
+
+        private void UpdateLocationSqlTable(short id, string name, short? cost, decimal? availability, string dateTimeStr)
+        {
+
+            using (var connection = new SqlConnection(connectionString))
+            {
+
+                MessageBox.Show(connection.ToString());
+                var command = new SqlCommand("USE " + dbName, connection);
+                connection.Open();
+                command.ExecuteNonQuery();
+
+                ConstructUpdateQueryByParameters(name, cost, availability, dateTimeStr, command);
+                command.CommandText += " WHERE LocationId=@id ";
+                command.Parameters.Add("@id", SqlDbType.SmallInt).Value = id;
+                try
+                {
+                    var numRows = command.ExecuteNonQuery();
+                    MessageBox.Show("Aggiornamento tabella avvenuto correttamente righe aggiornate " + numRows);
+                }
+                catch (SqlException e)
+                {
+                    MessageBox.Show("Errore " + e.Message);
+                }
+                this.DisplayLocationTable();
+            }
+
+        }
+        private void ConstructUpdateQueryByParameters(string name, Int16? cost, decimal? availabilty, string dateTimeStr, SqlCommand command)
+        {
+            bool addComa = true;
+
+            if (!string.IsNullOrEmpty(name) && !cost.HasValue && !availabilty.HasValue && string.IsNullOrEmpty(dateTimeStr))
+            {
+                addComa = false;
+            }
+            if (!string.IsNullOrEmpty(name) && cost.HasValue && !availabilty.HasValue && !string.IsNullOrEmpty(dateTimeStr))
+            {
+                addComa = false;
+            }
+            if (string.IsNullOrEmpty(name) && !cost.HasValue && availabilty.HasValue && !string.IsNullOrEmpty(dateTimeStr))
+            {
+                addComa = false;
+            }
+            if (string.IsNullOrEmpty(name) && cost.HasValue && !availabilty.HasValue && !string.IsNullOrEmpty(dateTimeStr))
+            {
+                addComa = false;
+            }
+
+            if (string.IsNullOrEmpty(name) && cost.HasValue && !availabilty.HasValue && !string.IsNullOrEmpty(dateTimeStr))
+            {
+                addComa = false;
+            }
+
+            StringBuilder builder = new StringBuilder("UPDATE Production.Location SET ");
+            if (!string.IsNullOrEmpty(name))
+            {
+                builder.Append("name = @name ");
+
+
+
+                command.Parameters.Add("@name", SqlDbType.NVarChar, 50).Value = name;
+            }
+
+
+            if (cost.HasValue)
+            {
+                if (addComa)
+                {
+                    builder.Append(", ");
+                }
+                builder.Append("costRate = @cost ");
+                command.Parameters.Add("@cost", SqlDbType.SmallMoney).Value = cost;
+
+            }
+            if (availabilty.HasValue)
+            {
+                if (addComa)
+                {
+                    builder.Append(", ");
+                }
+                builder.Append("availability = @av ");
+                command.Parameters.Add("@av", SqlDbType.Decimal).Value = availabilty;
+            }
+            DateTime dateTime;
+            if (!string.IsNullOrEmpty(dateTimeStr) && DateTime.TryParse(dateTimeStr, out dateTime))
+            {
+                if (addComa)
+                {
+                    builder.Append(", ");
+                }
+                builder.Append("modifiedDate = @mod");
+
+                command.Parameters.Add("@mod", SqlDbType.DateTime).Value = dateTime;
+            }
+
+            command.CommandText = builder.ToString();
+        }
+
+        private string GetNameById(short id)
+        {
+            string name = "";
+            using (var connection = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand("USE " + dbName, connection);
+                connection.Open();
+                command.ExecuteNonQuery();
+                command.CommandText = "SELECT name FROM Production.Location WHERE LocationId=@id";
+                command.Parameters.Add("@id", SqlDbType.SmallInt).Value = id;
+                try
+                {
+                    var reader = command.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        name = reader.GetString(0);
+                    }
+                }
+                catch (SqlException e)
+                {
+                    MessageBox.Show("Errore GetNameById " + e.Message);
+                    Application.Exit();
+                }
+
+
+
+            }
+            return name;
+        }
+
+
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var name = nameTxt.Text;
+            var costRateStr = costRateTxt.Text;
+            var availibiltyStr = availabilyTxt.Text;
+            var dateTimeStr = dateTimePicker1.Text;
+            DateTime dateTime;
+
+            if (!string.IsNullOrEmpty(name) && !string.IsNullOrEmpty(costRateStr) && !string.IsNullOrEmpty(dateTimeStr) && DateTime.TryParse(dateTimeStr, out dateTime))
+            {
+                try
+                {
+
+                    var idLocation = AddLocationIntoLocationSqlTable(nameTxt.Text, Convert.ToInt16(costRateTxt.Text), Convert.ToDecimal(availabilyTxt.Text), dateTimePicker1.Value);
+                    MessageBox.Show("Inserimento OK");
+                }
+                catch(SqlException ex)
+				{
+                    MessageBox.Show("Errore Inserimento campo name duplicato");
+                    Debug.WriteLine("Errore "+ ex.Message);
+				}
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Errore " + ex.Message);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Errore dati moncanti per l'inserimento");
+            }
+
+
+            DisplayLocationTable();
+
+
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            DeleteLocation(Convert.ToInt16(idTxt.Text));
+            DisplayLocationTable();
+
+        }
+	}
+}
+
